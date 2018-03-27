2e04b2992c64d2cac06a2d027e39e5bb794e83f1
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-03-27 18:08:57 +0300

    Papildimas prie JS failo

--- #9 Web CV  (advanced)/js/script.js
+++ #9 Web CV  (advanced)/js/script.js
@@ -7,17 +7,21 @@ function send(){
     // patikrinam varda
     if(vardas.length > 0){
         document.getElementById("firstname").classList.remove("is-invalid");
+        document.getElementById("firstname_error_msg").innerText = "";
     }else{
         document.getElementById("firstname").classList.add("is-invalid");
+        document.getElementById("firstname_error_msg").innerText = "Vardas yra provalomas";
     }
 
     // patikrinam pavarde
     if(pavarde.length > 0){
         // Kai pavarde yra taisinga
         document.getElementById("lastname").classList.remove("is-invalid");
+        document.getElementById("lastname_error_msg").innerText = "";
     }else{
         //Kai pavarde NERA taisinga
         document.getElementById("lastname").classList.add("is-invalid");
+        document.getElementById("lastname_error_msg").innerText = "Pavarde yra provaloma";
     }
 
     // patikrinam el.pasta
@@ -26,22 +30,27 @@ function send(){
         if (elPastas.match(/[\w-]+@([\w-]+\.)+[\w-]+/g)) {
             //console.log("El pastas yra taisingas");
             document.getElementById("email").classList.remove("is-invalid");
+            document.getElementById("email_error_msg").innerText = "";
         }else{
-            // Kai 
+            // Kai el pastas yra neteisingas
             document.getElementById("email").classList.add("is-invalid");
+            document.getElementById("email_error_msg").innerText = "Blogai suformatuotas el. pastas";
         }
     }else{
         // Kai el. pastas YRA TUSCIAS
         document.getElementById("email").classList.add("is-invalid");
+        document.getElementById("email_error_msg").innerText = "El. pastas yra privalomas";
     }
 
     // patikrinam varda
     if(pavarde.length > 0){
         //Kai zinute yra tvarkinga
         document.getElementById("message").classList.remove("is-invalid");
+        document.getElementById("message_error_msg").innerText = "";
     }else{
         //Kai zinute YRA TUSCIA
         document.getElementById("message").classList.add("is-invalid");
+        document.getElementById("message_error_msg").innerText = "Zinute yra privaloma";
     }
 
 }
