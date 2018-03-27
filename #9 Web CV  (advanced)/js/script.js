efa5784d36cf065a2062ac81bcf1d03d23237eb8
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-03-27 16:52:17 +0300

    Pridedam Regular expression

--- #9 Web CV  (advanced)/js/script.js
+++ #9 Web CV  (advanced)/js/script.js
@@ -1,3 +1,37 @@
 function send(){
-    alert("Zinute isiusta!");
+    let vardas = document.getElementById("firstname").value;
+    let pavarde = document.getElementById("lastname").value;
+    let elPastas = document.getElementById("email").value;
+    let zinute = document.getElementById("message").value;
+
+    console.log(vardas);
+    console.log(pavarde);
+    console.log(elPastas);
+    console.log(zinute);
+
+    if(vardas.length > 0){
+        console.log("Vardas yra taisingas");
+    }else{
+        alert("Vardas yra provalomas");
+    }
+
+    if(pavarde.length > 0){
+        console.log("Pavarde yra taisinga");
+    }else{
+        alert("Pavarde yra provaloma");
+    }
+
+    if(elPastas.length > 0){        
+        var re = new RegExp("[\w-]+@([\w-]+\.)+[\w-]+");
+        if (re.test(elPastas)) {
+            console.log("El pastas yra taisingas");
+        } else {
+            alert("El pasto forma neteisinga");
+        }
+    }else{
+        alert("El pastas yra provalomas");
+    }
+
+    
+
 }
