3712076012cbab2f7c37d55a1aea5df0ee3071ad
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-03-29 17:51:42 +0300

    added separator

--- #12 JQuery Intro/js/script.js
+++ #12 JQuery Intro/js/script.js
@@ -3,4 +3,18 @@ $("#test").click(function () {
 });
 
 
+$("#FadeOut").click(function () {
+    $("#papugos").fadeOut(5000);
+});
 
+$("#FadeIn").click(function () {
+    $("#papugos").fadeIn(5000);
+});
+
+$("#Example1_button").click(function () {
+    //get Example 1 text
+    let t1 = $("#textExample1_text").val();
+    console.log(t1);
+    //add text to Example 1 text result
+    $("#textExample1_result").text(t1);
+});
