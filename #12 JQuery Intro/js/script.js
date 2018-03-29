34b821ec2c5a760a0abd6803cbca35438079efa8
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-03-29 18:46:54 +0300

    Ikelem patobulinimus....

--- #12 JQuery Intro/js/script.js
+++ #12 JQuery Intro/js/script.js
@@ -12,9 +12,21 @@ $("#FadeIn").click(function () {
 });
 
 $("#Example1_button").click(function () {
-    //get Example 1 text
-    let t1 = $("#textExample1_text").val();
-    console.log(t1);
-    //add text to Example 1 text result
+    let t1 = $("#textExample1_text").val();    
     $("#textExample1_result").text(t1);
+});
+
+
+$("#Example2_button").click(function () {
+    let t1 = $("#textExample2_text1").val();
+    let t2 = $("#textExample2_text2").val();
+
+    let merged_text = t1 + " " + t2;
+
+    $("#textExample2_result").text(merged_text);
+});
+
+$("#textExample3").keyup(function () {
+    let t1 = $("#textExample3").val();
+    $("#textExample3_result").text(t1);
 });
